ce4c2e7a70d8e2a40d466a3e0e6e717b8b930dd1
Mario Nebl <hello@mario-nebl.de>
2017-12-24 11:04:49 +0100

    style: reformat code for readability

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,45 +1,39 @@
-import {guessTerminal} from '.';
+import { guessTerminal } from ".";
 
 const env = Object.assign({}, process.env);
 
 beforeAll(() => {
-    process.env = {};
+  process.env = {};
 });
 
 afterAll(() => {
-    process.env = env;
+  process.env = env;
 });
 
-it('exists', () => {
-    expect(typeof guessTerminal).toBe('function');
+it("exists", () => {
+  expect(typeof guessTerminal).toBe("function");
 });
 
-it('returns expected value terminal', () => {
-    process.env.TERM_PROGRAM = 'Apple_Terminal';
+it("returns expected value terminal", () => {
+  process.env.TERM_PROGRAM = "Apple_Terminal";
 
-    const expected = process.platform === 'darwin'
-        ? 'terminal'
-        : null;
+  const expected = process.platform === "darwin" ? "terminal" : null;
 
-    expect(guessTerminal()).toBe(expected);
+  expect(guessTerminal()).toBe(expected);
 });
 
-it('returns expected value iterm2', () => {
-    process.env.TERM_PROGRAM = 'iterm';
+it("returns expected value iterm2", () => {
+  process.env.TERM_PROGRAM = "iterm";
 
-    const expected = process.platform === 'darwin'
-        ? 'iterm2'
-        : null;
+  const expected = process.platform === "darwin" ? "iterm2" : null;
 
-    expect(guessTerminal()).toBe(expected);
+  expect(guessTerminal()).toBe(expected);
 });
 
-it('returns expected value hyper', () => {
-    process.env.TERM_PROGRAM = 'Hyper';
+it("returns expected value hyper", () => {
+  process.env.TERM_PROGRAM = "Hyper";
 
-    const expected = process.platform === 'darwin'
-        ? 'hyper'
-        : null;
+  const expected = process.platform === "darwin" ? "hyper" : null;
 
-    expect(guessTerminal()).toBe(expected);
+  expect(guessTerminal()).toBe(expected);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-const os = require('os');
+import * as os from "os";
 
 export enum GuessedTerminal {
-    hyper = 'hyper',
-    iterm2 = 'iterm2',
-    terminal = 'terminal'
+  hyper = "hyper",
+  iterm2 = "iterm2",
+  terminal = "terminal"
 }
 
 export function guessTerminal(): GuessedTerminal | null {
-    if (os.platform() !== 'darwin') {
-        return null;
-    }
+  if (os.platform() !== "darwin") {
+    return null;
+  }
 
-    switch (process.env.TERM_PROGRAM) {
-        case 'Apple_Terminal':
-            return GuessedTerminal.terminal;
-        case 'Hyper':
-            return GuessedTerminal.hyper;
-        case 'iterm':
-            return GuessedTerminal.iterm2;
-        default:
-            return null;
-    }
+  switch (process.env.TERM_PROGRAM) {
+    case "Apple_Terminal":
+      return GuessedTerminal.terminal;
+    case "Hyper":
+      return GuessedTerminal.hyper;
+    case "iterm":
+      return GuessedTerminal.iterm2;
+    default:
+      return null;
+  }
 }
